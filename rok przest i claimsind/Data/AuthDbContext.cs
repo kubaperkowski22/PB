e4eab9a45a723eada66353dfdddd6c96e54600ff
Jakub Perkowski <107010376+kubaperkowski22@users.noreply.github.com>
2023-06-06 17:54:36 +0200

    Add files via upload

---
+++ rok przest i claimsind/Data/AuthDbContext.cs
@@ -0,0 +1,35 @@
+﻿
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration.UserSecrets;
+
+namespace LeapYearApp.Data
+{
+    public class AuthDbContext : IdentityDbContext
+    {
+        public AuthDbContext(DbContextOptions<AuthDbContext> options) : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+
+            var userRoleId = "442b0988-f961-4728-97cc-6edd67ffbd87";
+
+            var roles = new List<IdentityRole>
+            {
+                new IdentityRole()
+                {
+                    Name = "User",
+                    NormalizedName = "User",
+                    Id = userRoleId,
+                    ConcurrencyStamp = userRoleId
+                }
+            };
+
+            builder.Entity<IdentityRole>().HasData(roles);
+        }
+    }
+}
---
+++ rok przest i claimsind/Data/LeapYearAppDbContext.cs
@@ -0,0 +1,14 @@
+﻿using LeapYearApp.Models.Domain;
+using Microsoft.EntityFrameworkCore;
+
+namespace LeapYearApp.Data
+{
+    public class LeapYearAppDbContext : DbContext
+    {
+        public LeapYearAppDbContext(DbContextOptions<LeapYearAppDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<YearNameForm> YearNameForms { get; set; }
+    }
+}
